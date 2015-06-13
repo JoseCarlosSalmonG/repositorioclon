99eb9b562d6904cf5627814a000e8cb87dec8f11
jsilvab <jsilvab.desarrolloweb@gmail.com>
2015-06-13 02:02:57 +0000

    asociando ciudades js

--- Reservas/config/routes.rb
+++ Reservas/config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
   resources :cities
 
   get 'home/index'
-
+  root 'home#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
